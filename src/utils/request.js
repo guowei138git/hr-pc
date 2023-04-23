47fe2007017519e16bac8f36ff0d0bec587da8a0
disanyanshijie.gw <908592508@qq.com>
2023-04-23 18:10:48 +0800

    7.0_区分axios在不同环境中的请求基础地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,13 @@
 //导出 一个axios的实例 而且这个实例要有请求拦截器和响应拦截器
 import axios from 'axios' // 引入 axios
-const service = axios.create() // 创建一个axios的实例
+
+// 创建一个axios的实例
+const service = axios.create({
+    // 当执行 npm run dev => .env.development => /api
+    // 当执行 npm run build => .env.production => /prod-api
+    baseURL: process.env.VUE_APP_BASE_API
+}) 
+
 service.interceptors.request.use() // 请求拦截器
 service.interceptors.response().use() // 响应拦截器
 export default service // 导出axios实例
