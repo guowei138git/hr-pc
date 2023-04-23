e1c56b4ac6bb36eae1d58100dcbb8a0395bf4f81
disanyanshijie.gw <908592508@qq.com>
2023-04-23 16:59:49 +0800

    5.1_封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,18 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
+/**
+ * 登录接口封装
+ * @param {*} data 
+ */
 export function login(data) {
-  
+  // 返回一个axios对象 => promise
+  return request({
+      // 因为所有的接口都要跨域，表示所有的接口都带 /api
+      url: 'sys/login',
+      method: 'post',
+      // 请求体参数
+      data
+  })
 }
 
 export function getInfo(token) {
