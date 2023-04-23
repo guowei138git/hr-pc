deedb2c6cb46845efeac71687999fa26572c030b
disanyanshijie.gw <908592508@qq.com>
2023-04-23 18:34:06 +0800

    6.1_封装Vuex的登录Aciton

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,16 @@
+// 状态
+const state = {
+  // 设置token的共享状态
+  token:null
+}
+// 修改状态
+const mutations = {}
+// 执行异步
+const actions = {}
+
 export default {
   namespaced: true,
-  state: {
-    
-  },
-  mutations: {
-    
-  },
-  actions: {
-    
-  }
+  state,
+  mutations,
+  actions
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,26 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+// 设置一个独一无二的 key 
+const TokenKey = 'hrsaas-ihrm-token'
 
+/**
+ * 获取token
+ */
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
+/**
+ * 存储token
+ * @param {*} token 
+ */
 export function setToken(token) {
   return Cookies.set(TokenKey, token)
 }
 
+/**
+ * 删除token
+ */
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
