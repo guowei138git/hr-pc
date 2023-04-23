b47eb3e820a41bbbf43263ac72db127abf3f8a97
disanyanshijie.gw <908592508@qq.com>
2023-04-23 21:00:05 +0800

    7.1_处理axios的响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 //导出 一个axios的实例 而且这个实例要有请求拦截器和响应拦截器
 import axios from 'axios' // 引入 axios
+import { Message } from 'element-ui'
 
 // 创建一个axios的实例
 const service = axios.create({
@@ -11,5 +12,26 @@ const service = axios.create({
 }) 
 
 service.interceptors.request.use() // 请求拦截器
-service.interceptors.response().use() // 响应拦截器
+
+// 响应拦截器
+service.interceptors.response().use(response => {
+    // 成功 -> 解构数据
+    const {success, message, data} = response.data
+    // 判断 success 
+    if (success) {
+        // 是 成功 -> 返回data
+        return data
+    } else {
+        // 否 失败 -> 提示错误
+        Message.error(message)
+        // 返回 reject
+        return Promise.reject(new Error(message))
+    }
+}, error =>{
+    // 失败 -> 提示错误
+    Message.error(error.message)
+    // reject 返回执行错误 让当前的执行链跳出成功 直接进入 catch
+    return Promise.reject(error)
+}) 
+
 export default service // 导出axios实例
