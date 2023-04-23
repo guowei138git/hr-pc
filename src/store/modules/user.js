11309c3ec105b43915dbd1a7bcbd08436bfc64dd
disanyanshijie.gw <908592508@qq.com>
2023-04-23 19:00:52 +0800

    6.3_封装登录的action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
 
 
 // 状态
@@ -31,7 +32,22 @@ const mutations = {
   
 }
 // 执行异步
-const actions = {}
+const actions = {
+  // 定义 login action 
+  async login(context, data){
+    // 实际上返回的就是一个 promise对象
+    // result就是执行的结果
+    const result = await login(data)
+    // axios默认给数据 加了一层 data
+    if(result.data.success){
+      // 表示登录接口调用成功 也就意味着你的用户名和密码是正确的
+      // 获取用户的token
+      const token = result .data.data
+      // 由于actions 修改state 必须通过mutations
+      context.commit('setToken', token)
+    }
+  }
+}
 
 export default {
   namespaced: true,
