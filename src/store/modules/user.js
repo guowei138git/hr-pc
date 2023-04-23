5c647a909d0b1c3797d005f27f1f92375ae0d0d3
disanyanshijie.gw <908592508@qq.com>
2023-04-23 18:47:09 +0800

    提供修改token的mutations -  修改和删除token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,35 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+
+
 // 状态
+// 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
+// Vuex的持久化如何实现呢？ Vuex和前端缓存相结合 
 const state = {
   // 设置token的共享状态
-  token:null
+  // 设置token初始状态 token持久化 => 放到缓存中(浏览器)
+  token:getToken()
 }
 // 修改状态
-const mutations = {}
+const mutations = {
+  // 修改token
+  setToken(state, token){
+    // 设置token 只是修改state的数据
+    // 例如： 123 -> 1234
+    state.token = token
+    // vuex变化 => 缓存数据
+    // vuex和 缓存数据的同步
+    setToken(token)
+  },
+  // 删除token
+  removeToken(state){
+    // 删除vuex中的token
+    state.token = null
+    // 先清除 vuex  再清除缓存
+    // 实现 vuex 和 缓存数据的同步
+    removeToken()
+  }
+  
+}
 // 执行异步
 const actions = {}
 
