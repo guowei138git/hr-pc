41998990028910bbda93dbe567727678c93d82a0
disanyanshijie.gw <908592508@qq.com>
2023-04-25 15:45:49 +0800

    5.4_建立用户名的快捷访问

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,7 @@ const getters = {
   device: state => state.app.device,
   // 快捷访问
   // 在根级的getters上  开放子模块的属性给别人用
-  token:state => state.user.token
+  token:state => state.user.token,  // 建立token的快捷访问
+  name: state => state.user.userInfo.username // 建立对于用户名的快捷访问
 }
 export default getters
