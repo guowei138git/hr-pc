2a491222cecfe21644840c7566f3d81cde15ab4e
disanyanshijie.gw <908592508@qq.com>
2023-04-27 06:08:52 +0800

    2.1_业务模块页面的搭建

---
+++ src/views/departments/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h2>组织架构</h2></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/setting/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h2>
+          公式设置
+      </h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
