016d9f446b0a7df6b32b4e1b301a00b66c120082
disanyanshijie.gw <908592508@qq.com>
2023-04-27 18:57:46 +0800

    1.5_完善树形的静态组织架构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -34,7 +34,43 @@
           </el-col>
         </el-row>
         <!-- 放置一个el-tree -->
-        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true"></el-tree>
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
+          <!-- 123 -->
+          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据 
+          data 每个节点的数据对象-->
+          <el-row slot-scope="{ data }"
+          type="flex" 
+          justify="space-between" 
+          align="middle" 
+          style="height: 40px; width: 100%"
+          >
+          <el-col>
+            <!-- 左侧内容 -->
+            <span>{{data.name}}</span>
+          </el-col>
+          <el-col :span="4">
+            <!-- 右侧内容 -->
+            <el-row type="flex" justify="end">
+              <el-col>{{data.manager}}</el-col>
+              <el-col>
+                <!-- 放置下拉菜单 -->
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作
+                    <i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加部门</el-dropdown-item>
+                    <el-dropdown-item>编辑部门</el-dropdown-item>
+                    <el-dropdown-item>删除部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+        </el-tree>
       </el-card>
     </div>
   </div>
@@ -46,14 +82,16 @@ export default {
     return {
       departs: [
         {
-          name: "总裁办",
-          children: [{ name: "董事会" }]
+          name: "总裁办", manager:'曹操',
+          children: [{ name: "董事会", manager:'曹丕' }]
         },
         {
-          name: "行政部"
+          name: "行政部",
+          manager:'刘备'
         },
         {
-          name: "人事部"
+          name: "人事部",
+          manager:'孙权'
         }
       ],
       defaultProps: {
