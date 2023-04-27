23e68715e0f512e3d7618066eeafaf15f72f1a30
disanyanshijie.gw <908592508@qq.com>
2023-04-27 21:09:33 +0800

    2.1_封装单独的树操作栏组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+    <el-col>
+      <!-- 左侧内容 -->
+      <span>{{treeNode.name}}</span>
+    </el-col>
+    <el-col :span="4">
+      <!-- 右侧内容 -->
+      <el-row type="flex" justify="end">
+        <el-col>{{treeNode.manager}}</el-col>
+        <el-col>
+          <!-- 放置下拉菜单 -->
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              操作
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加部门</el-dropdown-item>
+              <el-dropdown-item>编辑部门</el-dropdown-item>
+              <el-dropdown-item>删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  //定义传入的树形
+  treeNode: {
+    required: true,
+    type: Object
+  }
+};
+</script>
+
+<style>
+</style>
