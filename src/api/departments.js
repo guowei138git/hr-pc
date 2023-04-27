f1d97f07811ce721370fd10146a2169b724726f3
disanyanshijie.gw <908592508@qq.com>
2023-04-28 07:51:07 +0800

    4.1_封装删除部门接口

--- src/api/departments.js
+++ src/api/departments.js
@@ -8,3 +8,21 @@ export function getDepartments(){
         url:'/company/department'
     })
 }
+
+/**
+ * 封装删除组织架构的部门-api
+ */
+export function delDepartments(id){
+    return request({
+        url:`/company/department/${id}`,
+        method:'delete'
+        // 接口满足 restful规范
+        /**
+         * 同样的地址 不同的方法  执行不同的业务
+         * delete 删除业务
+         * get 获取业务
+         * post 新增或添加业务
+         * put 修改业务
+         */
+    })
+}
