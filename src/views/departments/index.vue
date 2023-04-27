4891ea3b6282e49cbb5e99e640286c3a66f8aac7
disanyanshijie.gw <908592508@qq.com>
2023-04-27 09:38:30 +0800

    1.2_实现组织架构的头部内容

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,14 +1,50 @@
 <template>
   <div>
-      <h2>组织架构</h2></div>
+    <div class="app-container">
+      <!-- 组织架构内容 - 头部 -->
+      <el-card class="tree-card">
+        <!-- 放置结构内容 -->
+        <el-row type="flex" justify="space-between">
+          <el-col>
+            <!-- 左侧内容 -->
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <!-- 右侧内容 -->
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 放置下拉菜单 -->
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作
+                    <i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加部门</el-dropdown-item>
+                    <el-dropdown-item>黄金糕</el-dropdown-item>
+                    <el-dropdown-item>狮子头</el-dropdown-item>
+                    <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                    <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                    <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
