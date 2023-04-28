cff2476faaef8416e308d2aff3ce96c5e3c4c10f
disanyanshijie.gw <908592508@qq.com>
2023-04-28 22:14:22 +0800

    6.2_配置新增表单的基本校验规则

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -16,7 +16,13 @@
       </el-form-item>
 
       <el-form-item label="部门介绍" prop="introduce">
-        <el-input v-model="formData.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+        <el-input
+          v-model="formData.introduce"
+          style="width:80%"
+          placeholder="1-300个字符"
+          type="textarea"
+          :rows="3"
+        />
       </el-form-item>
     </el-form>
     <!-- 确定和取消 -->
@@ -48,8 +54,22 @@ export default {
         introduce: "" // 部门介绍
       },
       // 定义校验规则
-      rules:{
-
+      rules: {
+        name: [
+            {required:true,message:'部门名称不能为空',trigger:'blur'},
+            {min:1,max:50,message:'部门名称要求1-50个字符',trigger:'blur'}
+        ],
+        code:[
+            {required:true,message:'部门编码不能为空',trigger:'blur'},
+            {min:1,max:50,message:'部门编码要求1-50个字符',trigger:'blur'}
+        ],
+        manager:[
+            {required:true,message:'部门负责人不能为空',trigger:'blur'}
+        ],
+        introduce:[
+            {required:true,message:'部门介绍不能为空',trigger:'blur'},
+            {min:1,max:300,message:'部门介绍要求1-300个字符'}
+        ]
       }
     };
   }
