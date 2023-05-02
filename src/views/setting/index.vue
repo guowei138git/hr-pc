4e5d301f66a838e57fd3815d36c3f6b191e648a2
disanyanshijie.gw <908592508@qq.com>
2023-05-02 18:24:50 +0800

    4.3_form表单添加校验规则rules

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -11,7 +11,7 @@
               <el-button icon="el-icon-plus" type="primary" size="small">新增角色</el-button>
             </el-row>
             <!-- 给表格绑定数据 -->
-            <el-table border="" :data="list">
+            <el-table border :data="list">
               <el-table-column label="序号" width="120" align="center" type="index" />
               <el-table-column label="名称" width="240" align="center" prop="name" />
               <el-table-column label="描述" align="center" prop="description" />
@@ -22,23 +22,22 @@
                   <el-button size="small" type="primary">编辑</el-button>
                   <el-button size="small" type="danger" @click="deleteRoleFn(row.id)">删除</el-button>
                 </template>
-                
               </el-table-column>
             </el-table>
             <!-- 放置分页组件 -->
             <el-row type="flex" justify="center" align="middle" style="height:60px">
-              <el-pagination 
-              :current-page="page.page"
-              :page-size="page.pagesize"
-              :total="page.total"
-              layout="prev,pager,next"
-              @current-change="changePage"
-               />
+              <el-pagination
+                :current-page="page.page"
+                :page-size="page.pagesize"
+                :total="page.total"
+                layout="prev,pager,next"
+                @current-change="changePage"
+              />
             </el-row>
           </el-tab-pane>
 
           <el-tab-pane label="公司信息">
-            <el-alert 
+            <el-alert
               title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
               :closable="false"
               type="info"
@@ -56,7 +55,13 @@
                 <el-input v-model="formData.mailbox" disabled style="400px"></el-input>
               </el-form-item>
               <el-form-item label="备注">
-                <el-input v-model="formData.remarks" type="textarea" :rows="3" disabled style="400px"></el-input>
+                <el-input
+                  v-model="formData.remarks"
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="400px"
+                ></el-input>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -65,7 +70,7 @@
     </div>
     <!-- 放置一个弹层组件 -->
     <el-dialog title="编辑部门" :visible="true">
-      <el-form label-width="120px">
+      <el-form :model="roleForm" :rules="rules" label-width="120px">
         <el-form-item label="角色名称">
           <el-input />
         </el-form-item>
@@ -85,68 +90,71 @@
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo, deleteRole } from '@/api/setting'
-import { mapGetters } from 'vuex';
+import { getRoleList, getCompanyInfo, deleteRole } from "@/api/setting";
+import { mapGetters } from "vuex";
 export default {
-  data () {
+  data() {
     return {
       // 承接数组
-      list:[],
+      list: [],
       // 放置页面分页 - 相关数据
-      page:{
-        page:1,
-        pagesize:10,
-        total:0 // 记录总数
+      page: {
+        page: 1,
+        pagesize: 10,
+        total: 0 // 记录总数
       },
       // 公司信息 - formData对象接收
       formData: {},
       // 控制 - 弹层显示
       showDialog: false,
-      roleForm:{
-        name:'',
-        description:''
+      roleForm: {
+        name: "",
+        description: ""
+      },
+      rules: {
+        name: [{ required: true, message: "角色名称不能为空", trigger: "blur" }]
       }
-    }
+    };
   },
   computed: {
-    ...mapGetters(['companyId'])
+    ...mapGetters(["companyId"])
   },
-  created () {
-    this.getRoleListFn()
-    this.getCompanyInfoFn()
+  created() {
+    this.getRoleListFn();
+    this.getCompanyInfoFn();
   },
   methods: {
-    async getRoleListFn(){
-      const {total, rows} = await getRoleList(this.page)
-      console.log('total=', total)
-      this.page.total = total
-      this.list = rows
+    async getRoleListFn() {
+      const { total, rows } = await getRoleList(this.page);
+      console.log("total=", total);
+      this.page.total = total;
+      this.list = rows;
     },
-    changePage(newPage){
+    changePage(newPage) {
       // newPage：当前点击的页面
-      console.log('newPage=', newPage)
+      console.log("newPage=", newPage);
       // 将当前页面赋值 给当前的最新页码
-      this.page.page = newPage
-      this.getRoleListFn()
+      this.page.page = newPage;
+      this.getRoleListFn();
     },
     // 获取公司信息
-    async getCompanyInfoFn(){
-      const companyId = this.companyId
-      console.log('companyId=', companyId)
-      this.formData = await getCompanyInfo(companyId)
+    async getCompanyInfoFn() {
+      const companyId = this.companyId;
+      console.log("companyId=", companyId);
+      this.formData = await getCompanyInfo(companyId);
     },
     // 删除角色
-    async deleteRoleFn(id){
+    async deleteRoleFn(id) {
       try {
         // 弹框 - 提示
-        await this.$confirm('确认删除该角色吗？')
+        await this.$confirm("确认删除该角色吗？");
         // 只有点击了 - 确定 -> 菜能进入到下方
-        await deleteRole(id) // 调用删除接口
+        await deleteRole(id); // 调用删除接口
         // 重新加载数据
-        this.getRoleListFn()
-        this.$message.success('删除角色成功')
+        this.getRoleListFn();
+        this.$message.success("删除角色成功");
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
   }
