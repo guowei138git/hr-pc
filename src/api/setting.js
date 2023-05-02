317e796874918efc43ffbbb9b011a3f6fe97289c
disanyanshijie.gw <908592508@qq.com>
2023-05-02 16:59:03 +0800

    4.1_封装编辑接口

--- src/api/setting.js
+++ src/api/setting.js
@@ -28,3 +28,24 @@ export function deleteRole(id){
         method:'delete'
     })
 }
+
+/**
+ * 读取角色详情
+ */
+export function getRoleDetail(id){
+    return request({
+        url:`/sys/role/${id}`,
+        method:'get'
+    })
+}
+
+/**
+ * 编辑-更新角色信息
+ */
+export function updateRole(data){
+    return request({
+        url:`/sys/role/${data.id}`,
+        method:'put',
+        data
+    })
+}
