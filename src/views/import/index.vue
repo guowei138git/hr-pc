38e783287fc6ee3b34420ddb026f266335ab91c2
disanyanshijie.gw <908592508@qq.com>
2023-05-06 06:15:11 +0800

    9.4_解决excel导入时间问题

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -27,7 +27,12 @@ export default {
         console.log(item)
         Object.keys(item).forEach(key => {
           console.log('key:', key)
-          userInfo[userRelatons[key]] = item[key]
+          if (userRelatons[key] === 'timeOfEntry' || userRelatons[key] === 'correctionTime') {
+            // 后端接口 限制了  不能是字符串 要求 - 时间类型
+            userInfo[userRelatons[key]] = new Date(this.formatDate(item[key]), '/')
+          } else {
+             userInfo[userRelatons[key]] = item[key]
+          }
           console.log('userInfo-key:', userRelatons[key])
           console.log('userInfo-value:', item[key])
         })
@@ -37,8 +42,21 @@ export default {
       console.log('arr:', arr)
       // 调用导入接口
       await importEmployee(arr)
+      this.$message.success('导入excel成功')
       // 返回上一页
       this.$router.back()
+    },
+    // 转化excel的日期格式
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
     }
   }
 
