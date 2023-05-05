b0859d552552178215d42d2062e77f601819148d
disanyanshijie.gw <908592508@qq.com>
2023-05-05 16:24:43 +0800

    9.1_建立公共导入的页面和路由

--- src/router/index.js
+++ src/router/index.js
@@ -60,6 +60,16 @@ export const constantRoutes = [
     }]
   },
 
+  {
+    path:'/import',
+    component:Layout,
+    hidden:true, // 不显示左侧菜单中
+    children:[{
+      path:'', // 什么都不写表示默认的二级路由
+      component:() => import('@/views/import')
+    }]
+  },
+  
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
---
+++ src/views/import/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- 公共导入组件 -->
+    公共导入组件123
+    <upload-excel />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
