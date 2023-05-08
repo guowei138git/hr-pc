b0dbb3725d4406b17786f839e9a5368b316993d6
huaheshang <908592508@qq.com>
2023-05-08 14:19:05 +0800

    3.1_权限点管理页面的开发

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-      <h2>
-          权限管理
-      </h2>
+  <div class="app-container">
+      <!-- 页面工具栏 -->
+      <page-tools>
+        <!-- 靠右的按钮 -->
+        <template v-slot:after>
+          <el-button type="primary" size="small">添加权限</el-button>
+        </template>
+      </page-tools>
   </div>
 </template>
 
